7c318e0c23f5b3624d9832b9ca8917f1e5677410
vantamin <vantablack.min@gmail.com>
2021-04-07 16:34:06 +0900

    Update Paperbase Theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Paperbase from 'pages/paperbase/Paperbase';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
+        <Route path="/paperbase" component={Paperbase} />
         <Route path="/" component={App} />
       </Switch>
     </Router>
