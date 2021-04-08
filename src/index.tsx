a1b563759b2497496c2c505840a4b5a0cbca18ec
vantamin <vantablack.min@gmail.com>
2021-04-08 12:26:29 +0900

    Update Onepirate Theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {
+  ForgotPassword,
+  Home,
+  Privacy,
+  SignIn,
+  SignUp,
+  Terms,
+} from 'pages/onepirate';
 import Paperbase from 'pages/paperbase/Paperbase';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,6 +18,12 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
+        <Route path="/onepirate/forgot-password" component={ForgotPassword} />
+        <Route path="/onepirate/home" component={Home} />
+        <Route path="/onepirate/privacy" component={Privacy} />
+        <Route path="/onepirate/sign-in" component={SignIn} />
+        <Route path="/onepirate/sign-up" component={SignUp} />
+        <Route path="/onepirate/terms" component={Terms} />
         <Route path="/paperbase" component={Paperbase} />
         <Route path="/" component={App} />
       </Switch>
---
+++ src/pages/onepirate/index.js
@@ -0,0 +1,17 @@
+export { default as ForgotPassword } from './ForgotPassword';
+export * from './ForgotPassword';
+
+export { default as Home } from './Home';
+export * from './Home';
+
+export { default as Privacy } from './Privacy';
+export * from './Privacy';
+
+export { default as SignIn } from './SignIn';
+export * from './SignIn';
+
+export { default as SignUp } from './SignUp';
+export * from './SignUp';
+
+export { default as Terms } from './Terms';
+export * from './Terms';
