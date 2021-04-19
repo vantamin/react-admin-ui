05ef6c7baa74ff295b3acbd0bdd70ba80180309b
vantamin <vantablack.min@gmail.com>
2021-04-19 17:51:26 +0900

    Update Storybook Preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { theme } from 'theme';
+
 export const parameters = {
+  controls: { expanded: true },
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+];
---
+++ src/theme/index.tsx
@@ -0,0 +1,3 @@
+export { default as theme } from './theme';
+
+export { default as withRoot } from './withRoot';
