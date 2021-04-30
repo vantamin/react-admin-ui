51c0b9f5b6cd0c464b9e694d72e6c5275050928d
vantamin <vantablack.min@gmail.com>
2021-04-30 14:47:26 +0900

    Refactor withRoot

--- src/theme/withRoot.js
+++ src/theme/withRoot.js
@@ -2,15 +2,17 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './theme';
 
-export default function withRoot(Component) {
-  function WithRoot(props) {
+const withRoot = (Component) => {
+  const WithRoot = (props) => {
     return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...props} />
       </ThemeProvider>
     );
-  }
+  };
 
   return WithRoot;
-}
+};
+
+export default withRoot;
